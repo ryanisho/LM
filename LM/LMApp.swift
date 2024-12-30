56ea60ea35e48d57f023aba154eaf94065cfbcc8
Ryan Ho <rh564@cornell.ed>
2024-12-30 04:29:37 -0500

    Initial Commit

---
+++ LM/LMApp.swift
@@ -0,0 +1,17 @@
+//
+//  LMApp.swift
+//  LM
+//
+//  Created by r on 12/30/24.
+//
+
+import SwiftUI
+
+@main
+struct LMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LMTests/LMTests.swift
@@ -0,0 +1,17 @@
+//
+//  LMTests.swift
+//  LMTests
+//
+//  Created by r on 12/30/24.
+//
+
+import Testing
+@testable import LM
+
+struct LMTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
