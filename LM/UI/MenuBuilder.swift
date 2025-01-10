0cb13bcda9a8319a509e4251f633761286ce7685
Ryan Ho <rh564@cornell.ed>
2025-01-09 19:09:38 -0500

    Started menu taskbar development

--- LM/UI/MenuBuilder.swift
+++ LM/UI/MenuBuilder.swift
@@ -5,3 +5,32 @@
 //  Created by r on 12/30/24.
 //
 
+import Cocoa
+
+class MenuBuilder {
+    private let launchAtLoginManager: LaunchAtLoginManager
+    private let lidStateMonitor: LidStateMonitor
+    
+    init(laucnAtLoginManager: LaunchAtLoginManager, lidStateMonitor: LidStateMonitor) {
+        self.launchAtLoginManager = laucnAtLoginManager
+        self.lidStateMonitor = lidStateMonitor
+    }
+    
+    func buildMenu() -> NSMenu {
+        let menu = NSMenu()
+        
+        // enable/disable toggle
+        let toggleItem = NSMenuItem (
+            title: "Enabled",
+            action: #selector(MenuBuilder.toggleEnabled(_:)),
+            keyEquivalent: ""
+        )
+        
+        toggleItem.target = self
+        toggleItem.state = lidStateMonitor.isEnabled ? .on : .off
+        menu.addItem(toggleItem)
+        
+        // launch at login option
+        
+    }
+}
--- LM/UI/StatusBarController.swift
+++ LM/UI/StatusBarController.swift
@@ -5,3 +5,10 @@
 //  Created by r on 12/30/24.
 //
 
+import Cocoa
+
+class StatusBarController {
+    private var statusItem: NSStatusItem!
+    private let menuBuilder: MenuBu
+}
+
