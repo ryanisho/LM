acc326f8da64f8050410a61da3d1da520a1b3385
Ryan Ho <rh564@cornell.ed>
2025-01-09 19:49:04 -0500

    Finished ProcessExecutor

--- LM/Utilities/Logger.swift
+++ LM/Utilities/Logger.swift
@@ -5,3 +5,4 @@
 //  Created by r on 12/30/24.
 //
 
+import os.log
--- LM/Utilities/ProcessExecutor.swift
+++ LM/Utilities/ProcessExecutor.swift
@@ -5,3 +5,38 @@
 //  Created by r on 12/30/24.
 //
 
+import Foundation
+
+struct ProcessExecutor {
+    struct ProcessResult {
+        let output: String
+        let error: String
+        let exitCode: Int32
+    }
+    
+    static func execute(command: String, arguments: [String]) -> ProcessResult {
+        let task = Process()
+        let outputPipe = Pipe()
+        let errorPipe = Pipe()
+        
+        task.launchPath = command
+        task.arguments = arguments
+        task.standardOutput = outputPipe
+        task.standardError = errorPipe
+        
+        task.launch()
+        task.waitUntilExit()
+        
+        let outputData = outputPipe.fileHandleForReading.readDataToEndOfFile()
+        let errorData = errorPipe.fileHandleForReading.readDataToEndOfFile()
+        
+        let output = String(data: outputData, encoding: .utf8) ?? ""
+        let error = String(data: errorData, encoding: .utf8) ?? ""
+        
+        return ProcessResult (
+            output: output,
+            error: error,
+            exitCode: task.terminationStatus
+        )
+    }
+}
