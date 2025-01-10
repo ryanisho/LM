0494a4781769090ee6611821a25ba11667952506
Ryan Ho <rh564@cornell.ed>
2025-01-09 19:38:45 -0500

    Implemented StatusBarController

--- LM/UI/StatusBarController.swift
+++ LM/UI/StatusBarController.swift
@@ -9,6 +9,28 @@ import Cocoa
 
 class StatusBarController {
     private var statusItem: NSStatusItem!
-    private let menuBuilder: MenuBu
+    private let menuBuilder: MenuBuilder
+    
+    init(menuBuilder: MenuBuilder) {
+        self.menuBuilder = menuBuilder
+        setupStatusBar()
+    }
+    
+    private func setupStatusBar() {
+        statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
+        
+        if let button = statusItem.button {
+            button.image = NSImage(systemSymbolName: "speaker.slash.circle.fill", accessibilityDescription: "LidMute")
+        }
+        
+        statusItem.menu = menuBuilder.buildMenu()
+    }
+    
+    func updateIcon(muted: Bool) {
+        if let button = statusItem.button {
+            let imageName = muted ? "speaker.slash.circle.fill" : "speaker.circle.fill"
+            button.image = NSImage(symbolName: imageName, accessibilityDescription: "LidMute")
+        }
+    }
 }
 
